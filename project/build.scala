ee2d3d1f3df48bca7dfde587a34cf62380615dba
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2018-03-31 00:14:17 +0200

    Update the logger dependencies (entirely switch to pure logback since customization beyond scala-logging is needed anyway).

--- project/build.scala
+++ project/build.scala
@@ -141,8 +141,7 @@ object ViperServerBuild extends Build {
   object dependencies {
     lazy val logging = Seq(
       "org.slf4j" % "slf4j-api" % "1.7.12",
-      "ch.qos.logback" % "logback-classic" % "1.1.7",
-      "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0")
+      "ch.qos.logback" % "logback-classic" % "1.2.3")
 
     lazy val scallop = "org.rogach" %% "scallop" % "2.0.7"
     lazy val jgrapht = "org.jgrapht" % "jgrapht-core" % "0.9.1"
