48a895cf518a6330bc1577b05f155b99dde74ca9
Alessio Aurecchia <alessio@aurecchia.ch>
2018-07-22 23:16:32 +0200

    Change "distinct" contents to symbols as they are not terms.

--- src/main/scala/viper/server/TermWriter.scala
+++ src/main/scala/viper/server/TermWriter.scala
@@ -96,7 +96,7 @@ object TermWriter {
     case Distinct(symbols) =>
       JsObject(
         "type" -> JsString("distinct"),
-        "terms" -> JsArray((symbols map (s => JsString(s.id.name))).toVector)
+        "symbols" -> JsArray((symbols map (s => JsString(s.id.name))).toVector)
       )
 
     case Ite(cond, thenBranch, elseBranch) =>
