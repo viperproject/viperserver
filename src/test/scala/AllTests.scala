7d8fe4bc358df240d3cf67d3294a461b3a59f117
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2019-04-12 00:34:47 +0200

    Implemented basic router tests for ViperServer.

--- src/test/scala/AllTests.scala
+++ src/test/scala/AllTests.scala
@@ -4,36 +4,96 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import org.scalatest.{ Matchers, WordSpec }
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.http.scaladsl.server._
-import akka.http.scaladsl.model._
-import Directives._
+import java.io.File
+import java.nio.file.Paths
+
+import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
+import akka.http.scaladsl.model.{StatusCodes, _}
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import akka.testkit.TestDuration
+import org.scalatest.{Matchers, WordSpec}
+import viper.server.{ViperRequests, ViperServerRunner}
+
+import scala.concurrent.duration._
 
 class ViperServerSpec extends WordSpec with Matchers with ScalatestRouteTest {
 
-  val emptyRoute =
-    get {
-      path("/") {
-        complete("OK")
-      }
-    }
+  import ViperRequests._
+  implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
+  implicit val requestTimeput: RouteTestTimeout = RouteTestTimeout(10.second dilated)
 
-  val exitRoute =
-    get {
-      path("exit") {
-        complete("")
-      }
+  ViperServerRunner.main(Array())
+
+  private val _routsUnderTest = ViperServerRunner.routes(ViperServerRunner.logger)
+
+  def printRequestResponsePair(req: String, res: String): Unit = {
+    println(s">>> ViperServer test request `$req` response in the following response: $res")
+  }
+
+  def getResourcePath(sil_file: String): String = {
+    val resource = getClass.getResource(sil_file)
+    val fname = if (resource != null) {
+      val file = Paths.get(resource.toURI)
+      file.toString
+    } else {
+      // simulate absent file
+      val temp_file = File.createTempFile("io_testing", ".sil")
+      val absent_fname = temp_file.getPath
+      temp_file.delete()
+      absent_fname
     }
+    fname
+  }
+
+  val verifiableFile = "all/basic/let.sil"
+  //val nonExistingFile = "bla/bla/bla.sil"
+  val emptyFile = "graphs/empty.vpr"
+
+  val tool = "silicon"
+  val testSimpleViperCodeWithSilicon_cmd = s"$tool ${getResourcePath(verifiableFile)}"
+  val testEmptyFileWithSilicon_cmd = s"$tool ${getResourcePath(emptyFile)}"
 
   "ViperServer" should {
-    "stop all running executions and terminate self" in {
-      // tests:
-      Get("/exit") ~> exitRoute ~> check {
-        status should ===(StatusCodes.OK)
 
-        contentType should ===(ContentTypes.`text/plain(UTF-8)`)
+    "start a verification session using Carbon over a small Viper program" in {
+      Post("/verify", VerificationRequest(testSimpleViperCodeWithSilicon_cmd)) ~> _routsUnderTest ~> check {
+        //printRequestResponsePair(s"POST, /verify, $testSimpleViperCodeWithSilicon_cmd", responseAs[String])
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+      }
+    }
+
+    "respond with the result for session #0" in {
+      Get("/verify/0") ~> _routsUnderTest ~> check {
+        //printRequestResponsePair(s"GET, /verify/0", responseAs[String])
+        responseAs[String] should include (""""kind":"overall","status":"success","verifier":"silicon"""")
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+      }
+    }
+
+    "start another verification session using Carbon " in {
+      Post("/verify", VerificationRequest(testEmptyFileWithSilicon_cmd)) ~> _routsUnderTest ~> check {
+        //printRequestResponsePair(s"POST, /verify, $testEmptyFileWithSilicon_cmd", responseAs[String])
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+      }
+    }
+
+    "respond with the result for session #1" in {
+      Get("/verify/1") ~> _routsUnderTest ~> check {
+        //printRequestResponsePair(s"GET, /verify/1", responseAs[String])
+        responseAs[String] should include (""""kind":"overall","status":"success","verifier":"silicon"""")
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
+      }
+    }
+
+    "stop all running executions and terminate self" in {
+      Get("/exit") ~> _routsUnderTest ~> check {
+        //printRequestResponsePair(s"GET, /exit", responseAs[String])
+        status should ===(StatusCodes.OK)
+        contentType should ===(ContentTypes.`application/json`)
       }
     }
   }
