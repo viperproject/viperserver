784f54dcf1552043d4ebed32936785dd55d8f432
Linard Arquint <linard.arquint@inf.ethz.ch>
2020-09-08 16:16:05 +0200

    adds and updates copyright headers

--- src/test/scala/CoreServerTests.scala
+++ src/test/scala/CoreServerTests.scala
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2011-2020 ETH Zurich.
+
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{Matchers, WordSpec}
@@ -8,6 +14,7 @@ import viper.silver.logger.SilentLogger
 import viper.silver.reporter._
 
 import scala.concurrent.Future
+
 import scala.util.{Failure, Success}
 
 class CoreServerTest extends WordSpec with Matchers with ScalatestRouteTest {
