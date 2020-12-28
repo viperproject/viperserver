a6a23da4c4cc066168c6a334a7d5d1829a23effd
Arshavir Ter-Gabrielyan <ter-gabrielyan@inf.ethz.ch>
2020-12-29 00:01:18 +0100

    Fixed the license headers

--- src/main/scala/viper/server/core/AstWorker.scala
+++ src/main/scala/viper/server/core/AstWorker.scala
@@ -1,5 +1,10 @@
-package viper.server.core
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2011-2020 ETH Zurich.
 
+package viper.server.core
 
 import ch.qos.logback.classic.Logger
 import viper.server.utility.AstGenerator
