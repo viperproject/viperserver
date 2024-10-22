a94d9ec2d7f2d9275ad343603ccd35aab93493c9
7i6ht <ruhstals@student.ethz.ch>
2024-10-22 17:07:05 +0200

    Make verification work

--- src/main/scala/viper/server/frontends/lsp/ClientCoordinator.scala
+++ src/main/scala/viper/server/frontends/lsp/ClientCoordinator.scala
@@ -208,7 +208,7 @@ class ClientCoordinator(val server: ViperServerService)(implicit executor: Verif
     // we use `computeIfAbsent` instead of `putIfAbsent` such that a new FileManager is only created if it's absent
     val fm = _files.computeIfAbsent(uri, createFMFunc)
     // Override the content if we are given one and the file manager was not just created
-    if (!createdNew && content.isDefined) fm.content.set(content.get)
+    if (!createdNew && content.isDefined && content.get != null) fm.content.set(content.get)
     fm
   }
   def ensureFmExists(uri: String, content: String): FileManager = {
