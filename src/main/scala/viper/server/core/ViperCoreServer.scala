2de38dce833788b436cf11897fd4534c3317b269
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2024-12-06 16:04:43 +0100

    Finish

--- src/main/scala/viper/server/core/ViperCoreServer.scala
+++ src/main/scala/viper/server/core/ViperCoreServer.scala
@@ -88,7 +88,14 @@ abstract class ViperCoreServer(val config: ViperConfig)(implicit val executor: V
             val program_maybe_fut: Future[Option[Program]] = handle.artifact
             program_maybe_fut.map(_.map(p => {
               val updated = verifyTarget match {
-                case Some(t) => p.copy(methods = Seq())(p.pos, p.info, p.errT)
+                // Make all methods abstract except for the selected method.
+                case Some(t) => p.copy(methods = p.methods.map(m => {
+                  if (m.name == t) {
+                    m
+                  } else {
+                    m.copy(body = None)(m.pos, m.info, m.errT)
+                  }
+                }))(p.pos, p.info, p.errT)
                 case None => p
               }
               new VerificationWorker(args, programId, updated, logger, config)(executor)
