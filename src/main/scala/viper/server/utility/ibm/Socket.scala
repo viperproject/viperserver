808959900600680293a9f50b298b53bcd9c2d007
Nicolas Klose <nicolas.klose@inf.ethz.ch>
2026-05-07 12:54:55 +0200

    H1+H2: Drop duplicate apache Socket and unused findFreePort

--- src/main/scala/viper/server/utility/ibm/Socket.scala
+++ src/main/scala/viper/server/utility/ibm/Socket.scala
@@ -33,48 +33,6 @@ object Socket {
   val MAX_PORT_NUMBER = 65535
 
   /**
-    * Returns a free port number on localhost.
-    *
-    * Heavily inspired from org.eclipse.jdt.launching.SocketUtil (to avoid a dependency to JDT just because of this).
-    * Slightly improved with close() missing in JDT. And throws exception instead of returning -1.
-    *
-    * Source: https://gist.github.com/vorburger/3429822
-    * TODO: check license
-    *
-    * @return a free port number on localhost
-    * @throws IllegalStateException if unable to find a free port
-    */
-  def findFreePort: Int = {
-    var socket: ServerSocket = null
-    try {
-      socket = new ServerSocket (0)
-      socket.setReuseAddress (true)
-      val port: Int = socket.getLocalPort
-      try {
-        socket.close ()
-      } catch {
-        case _: IOException =>
-
-        // Ignore IOException on close()
-      }
-      return port
-    } catch {
-      case _: IOException =>
-
-    } finally {
-      if (socket != null) {
-        try {
-          socket.close ()
-        } catch {
-          case _: IOException =>
-
-        }
-      }
-    }
-    throw new IllegalStateException ("Could not find a free TCP/IP port to start ViperServer on")
-  }
-
-  /**
     * Checks to see if a specific port is available.
     *
     * Source: http://svn.apache.org/viewvc/camel/trunk/components/camel-test/src/main/java/org/apache/camel/test/AvailablePortFinder.java
